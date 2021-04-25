411aac50c4ab73fc68337be13d66cfa175804e0f
G Roques <groques360@gmail.com>
2021-04-25 17:18:19 -0500

    Add unit test for translated link

--- tests/export_test.py
+++ tests/export_test.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from pathlib import Path
 
 import FreeCAD as App
 import freecad_to_obj
@@ -49,6 +50,38 @@ class FreeCADToObjExportTest(unittest.TestCase):
         obj_file_contents = freecad_to_obj.export([part])
 
         self.assertEqual(obj_file_contents, expected)
+
+    def test_export_with_translated_link(self):
+        test_package_path = Path(__file__).parent
+
+        cube_document = App.newDocument('Cube')
+        box = cube_document.addObject('Part::Box', 'Box')
+        box.Label = 'Cube'
+
+        cube_document_path = test_package_path.joinpath('Cube.FCStd')
+        cube_document.saveAs(str(cube_document_path))
+
+        link_document = App.newDocument('CubeLink')
+        link_document_path = test_package_path.joinpath('LinkCube.FCStd')
+        link_document.saveAs(str(link_document_path))
+        link = link_document.addObject('App::Link', 'Link')
+        link.setLink(box)
+        link.Label = box.Label
+
+        link.Placement = Placement(
+            Vector(10, 0, 0), Rotation(Vector(0, 0, 1), 0))
+
+        link_document.recompute()
+
+        with open(os.path.join(os.path.dirname(__file__), 'translated_cube.obj')) as f:
+            expected = f.read()
+
+        obj_file_contents = freecad_to_obj.export([link])
+
+        self.assertEqual(obj_file_contents, expected)
+
+        cube_document_path.unlink()
+        link_document_path.unlink()
 
 
 if __name__ == '__main__':
