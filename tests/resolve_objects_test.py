2e11c4d79f81896fc78d62bbc87b6b3d0908d596
G Roques <groques360@gmail.com>
2021-05-01 18:54:19 -0500

    Add test for part containing primitive

--- tests/resolve_objects_test.py
+++ tests/resolve_objects_test.py
@@ -20,7 +20,7 @@ class ResolveObjectsTest(unittest.TestCase):
         resolved_primitive, placement = resolved_objects[0]
         self.assertEqual(resolved_primitive.TypeId, 'Part::Box')
         self.assertEqual(resolved_primitive.Name, 'Box')
-        self.assertPlacementEqual(placement, primitive.Placement)
+        self.assertPlacementEqual(placement, Placement())
 
     def test_resolve_objects_with_translated_primitive(self):
         document = App.newDocument()
@@ -38,6 +38,22 @@ class ResolveObjectsTest(unittest.TestCase):
         self.assertEqual(resolved_primitive.Name, 'Box')
         self.assertPlacementEqual(placement, Placement(
             Vector(10, 0, 0), Rotation(Vector(0, 0, 1), 0)))
+
+    def test_resolve_objects_with_part_containing_primitive(self):
+        document = App.newDocument()
+        primitive = document.addObject('Part::Box', 'Box')
+        part = document.addObject('App::Part', 'Part')
+        part.addObject(primitive)
+        document.recompute()
+
+        resolved_objects = resolve_objects([part])
+
+        self.assertEqual(len(resolved_objects), 1)
+
+        resolved_primitive, placement = resolved_objects[0]
+        self.assertEqual(resolved_primitive.TypeId, 'Part::Box')
+        self.assertEqual(resolved_primitive.Name, 'Box')
+        self.assertPlacementEqual(placement, Placement())
 
     def assertPlacementEqual(self, a, b):
         self.assertAlmostEqual(a.Base.x, b.Base.x, places=3)
