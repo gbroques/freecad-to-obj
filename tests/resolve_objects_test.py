24bafefd05f156eea6aecd6fbaef04aa7b345386
G Roques <groques360@gmail.com>
2021-05-02 14:29:50 -0500

    Add test for link to transform link to shape

--- tests/resolve_objects_test.py
+++ tests/resolve_objects_test.py
@@ -190,6 +190,23 @@ class ResolveObjectsTest(unittest.TestCase):
         self.assertPlacementEqual(placement, Placement(
             Vector(18, 0, 0), Rotation()))
 
+    def test_resolve_objects_with_link_to_transform_link_to_shape(self):
+        link = (Assembler()
+                .link_to(Placement(Vector(7, 0, 0), Rotation()))
+                .transform_link_to(Placement(Vector(1, 0, 0), Rotation()))
+                .shape('Part::Box', 'Box', Placement(Vector(10, 0, 0), Rotation()))
+                .assemble())
+
+        resolved_objects = resolve_objects([link])
+
+        self.assertEqual(len(resolved_objects), 1)
+
+        resolved_shape, placement = resolved_objects[0]
+        self.assertEqual(resolved_shape.TypeId, 'Part::Box')
+        self.assertEqual(resolved_shape.Name, 'Box')
+        self.assertPlacementEqual(placement, Placement(
+            Vector(17, 0, 0), Rotation()))
+
     def assertPlacementEqual(self, a, b):
         self.assertAlmostEqual(a.Base.x, b.Base.x, places=3)
         self.assertAlmostEqual(a.Base.y, b.Base.y, places=3)
